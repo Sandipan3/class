ae97b52dfc916c8f9176a3c15316a94d8e1076cb
Sandipan Jha <80733598+Sandipan3@users.noreply.github.com>
2026-03-15 00:45:55 +0530

    Update DES_7.java

--- DES_7.java
+++ DES_7.java
@@ -1,31 +1,90 @@
-import javax.crypto.*;
-import javax.crypto.spec.*;
-
 public class DES_7 {
 
-    static byte[] encrypt(String msg, String key) throws Exception {
-        Cipher c = Cipher.getInstance("DES");
-        SecretKeySpec k = new SecretKeySpec(key.getBytes(), "DES");
-        c.init(Cipher.ENCRYPT_MODE, k);
-        return c.doFinal(msg.getBytes());
+    static int[] IP = {58,50,42,34,26,18,10,2,
+                       60,52,44,36,28,20,12,4,
+                       62,54,46,38,30,22,14,6,
+                       64,56,48,40,32,24,16,8,
+                       57,49,41,33,25,17,9,1,
+                       59,51,43,35,27,19,11,3,
+                       61,53,45,37,29,21,13,5,
+                       63,55,47,39,31,23,15,7};
+
+    static int[] FP = {40,8,48,16,56,24,64,32,
+                       39,7,47,15,55,23,63,31,
+                       38,6,46,14,54,22,62,30,
+                       37,5,45,13,53,21,61,29,
+                       36,4,44,12,52,20,60,28,
+                       35,3,43,11,51,19,59,27,
+                       34,2,42,10,50,18,58,26,
+                       33,1,41,9,49,17,57,25};
+
+    static String permute(String in, int[] table) {
+        String out = "";
+        for(int i : table)
+            out += in.charAt(i-1);
+        return out;
     }
 
-    static String decrypt(byte[] data, String key) throws Exception {
-        Cipher c = Cipher.getInstance("DES");
-        SecretKeySpec k = new SecretKeySpec(key.getBytes(), "DES");
-        c.init(Cipher.DECRYPT_MODE, k);
-        return new String(c.doFinal(data));
+    static String xor(String a, String b) {
+        String r="";
+        for(int i=0;i<a.length();i++)
+            r += (a.charAt(i)==b.charAt(i))?"0":"1";
+        return r;
     }
 
-    public static void main(String[] args) throws Exception {
+    static String round(String L, String R, String key) {
+        String f = xor(R,key.substring(0,32));
+        String newR = xor(L,f);
+        return R + newR;
+    }
 
-        String text = "Hello";
-        String key = "12345678";
+    // -------- Encryption --------
+    static String encrypt(String pt, String key){
 
-        byte[] enc = encrypt(text, key);
-        System.out.println("Encrypted: " + enc);
+        String ip = permute(pt,IP);
 
-        String dec = decrypt(enc, key);
-        System.out.println("Decrypted: " + dec);
+        String L = ip.substring(0,32);
+        String R = ip.substring(32);
+
+        for(int i=0;i<16;i++){
+            String res = round(L,R,key);
+            L = res.substring(0,32);
+            R = res.substring(32);
+        }
+
+        String combined = R + L;
+
+        return permute(combined,FP);
+    }
+
+    // -------- Decryption --------
+    static String decrypt(String cipher, String key){
+
+        String ip = permute(cipher,IP);
+
+        String L = ip.substring(0,32);
+        String R = ip.substring(32);
+
+        for(int i=15;i>=0;i--){
+            String res = round(L,R,key);
+            L = res.substring(0,32);
+            R = res.substring(32);
+        }
+
+        String combined = R + L;
+
+        return permute(combined,FP);
+    }
+
+    public static void main(String[] args) {
+
+        String pt  = "1100110011001100110011001100110011001100110011001100110011001100";
+        String key = "1010101010101010101010101010101010101010101010101010101010101010";
+
+        String cipher = encrypt(pt,key);
+        System.out.println("Cipher Text : " + cipher);
+
+        String decrypted = decrypt(cipher,key);
+        System.out.println("Decrypted Text : " + decrypted);
     }
 }
